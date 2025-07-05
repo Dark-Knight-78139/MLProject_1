a04578c277bfcdd1bc23b49c550900e827f6b00e
Dark-Knight-78139 <an.akhilnekkanti@gmail.com>
2025-07-05 11:39:14 +0530

    Trained the model using model_trainer method

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 from src.components.data_transformation import DataTransformationConfig, DataTransformation
 
 @dataclass
@@ -59,4 +61,7 @@ if __name__ == "__main__":
     logging.info("Data Ingestion completed successfully")
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_data, test_data,_= data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_data, test_data))
